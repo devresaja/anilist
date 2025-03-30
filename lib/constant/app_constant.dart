8a673c0278ebf73644f3601493ed48b910137864
devresaja <devresaja@gmail.com>
2025-03-30 13:15:23 +0700

    chore: add app constant

---
+++ lib/constant/app_constant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+  static const String webUrl = 'https://anilist-433306.firebaseapp.com';
+  static const String playstoreUrl =
+      'https://play.google.com/store/apps/details?id=com.anilist.android';
+  static const String privacyPolicy = '$webUrl/privacy_policy.html';
+  static const String deleteAccountGuide = '$webUrl/delete_account.html';
+}
